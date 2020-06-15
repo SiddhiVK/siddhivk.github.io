bf5cd0b3e8afc6caf27c9a39582c1ed86e478744
SiddhiVK <siddhikulk@gmail.com>
2020-06-15 20:46:46 +0530

    progress

--- js/main.js
+++ js/main.js
@@ -113,6 +113,110 @@ $(function(){
     }
 
 
+/*---------------------------------------------------------------------
+  Javascript Function For Sticky Navigation Bar AND SMOOTH SCROLLING
+----------------------------------------------------------------------*/
+
+    // Define stikyNav Function
+    function stikyNav() {
+
+      top = allWindow.scrollTop();
+
+      if ( top >= 100 ) {
+        navBar.addClass("nav-sticky");
+
+      } else {
+        navBar.removeClass("nav-sticky");
+      }
+
+      // SHow Also Scroll up Button
+      if ( top >= 1000 ) {
+        $('.scroll-up').addClass("show-up-btn");
+      } else {
+        $('.scroll-up').removeClass("show-up-btn");
+      }
+    }
+
+    // Select all links with hashes
+    $('a.scroll').on('click', function(event) {
+        // On-page links
+        if ( location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname ) {
+          // Figure out element to scroll to
+          var target = $(this.hash),
+              speed= $(this).data("speed") || 800;
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, speed);
+          }
+        }
+    });
+
+    $(".scroll-up").on('click', function (e) {
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 900);
+    });
+    
+
+/*---------------------------------------------------------------------
+  Javascript Function for Hide Navbar Dropdown After Click On Links
+-------------------------------------------------------------------*/
+
+    var navLinks = navBar.find(".navbar-collapse ul li a");
+
+    $.each( navLinks, function( i, val ) {
+
+      var navLink = $(this);
+
+        navLink.on('click', function (e) {
+          navBar.find(".navbar-collapse").collapse('hide');
+        });
+
+    });
+
+
+/*----------------------------------------------------------------
+  Javascript Function For Change active Class on navigation bar
+-----------------------------------------------------------------*/
+
+    var sections = $('.one-page-section'),
+        navList = navBar.find("ul.navbar-nav");
+
+    // Define ChangeClass Function
+    function ChangeClass() {
+
+      top = allWindow.scrollTop();
+
+        $.each(sections, function(i,val) {
+
+          var section = $(this),
+              section_top = section.offset().top - 10,
+              bottom = section_top + section.height();
+
+            if (top >= section_top && top <= bottom) {
+
+              var naItems = navList.find('li');
+
+              $.each(naItems ,function(i,val) {
+                var item = $(this);
+                item.find("a").removeClass("active");
+              });
+
+              navList.find('li [href="#' + section.attr('id') + '"]').addClass('active');
+            }
+
+        });
+
+    } // End of ChangeClass Function
+
+
 
 /*---------------------------------------------------
   Javascript Function FOR PARALLAX EFFECT
@@ -158,6 +262,57 @@ $(function(){
     allWindow.on('scroll', function() {
       scrollFunctions();
     });
+
+
+/*-----------------------------------------------------------------
+  Javascript Function for PROGRESS BAR LINES  SCRIPT
+------------------------------------------------------------------*/
+
+var linesHead = $(".skills-section"),
+line = $(".progress-bar-line");
+
+//Progress Bars function
+function progressFunction(e) {
+
+if ( linesHead.length ) {
+
+if (!linesHead.hasClass("done")) {
+
+  var linesHeadTop = linesHead.offset().top,
+      top = allWindow.scrollTop(),
+      winH = allWindow.height() - 160;
+
+  if (top >= linesHeadTop - winH) {
+
+    linesHead.addClass("done");
+    $.each( line, function( i, val ) {
+
+    var thisLine = $(this),
+      value = thisLine.data("percent"),
+      progressCont = $(thisLine).closest('.progress-bar-linear').find(".progress-cont span");
+
+      thisLine.css("width",value + "%");
+      progressCont.html(value + "%")
+
+    });
+  }
+}
+}
+} //End progressFunction Fuction
+
+
+function scrollFunctions() {
+stikyNav();
+ChangeClass();
+parallax();
+progressFunction();
+}
+
+// add Event listener to window
+allWindow.on('scroll', function() {
+scrollFunctions();
+});
+
 
 
 /*------------------------------------------
